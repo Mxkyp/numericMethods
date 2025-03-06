c109aca30c899f6170fab4f0d42a7568e25b2efe
mxkcpy <mikolaj19@poczta.fm>
2025-03-06 20:38:53 +0100

    created a basic tui

--- zad1/src/main.py
+++ zad1/src/main.py
@@ -3,14 +3,18 @@ import math
 
 class rootSolver:
 
-    def __init__(self, a: int, b: int, functionNr: int, precision: float):
+    def __init__(self, functionNr: int, a: int, b: int, 
+                 stopTypeNr: int, iterOrPrecision):
         assert self.areValidBounds(a, b)
 
+        self.functionNr = functionNr
         self.a = a
         self.b = b
-        self.functionNr = functionNr
-        self.precision = precision
-
+        self.stopType = stopTypeNr
+        if(stopTypeNr == 1):
+            self.precision = iterOrPrecision 
+        elif(stopTypeNr == 2):
+            self.iterCount = iterOrPrecision
     
     def solve():
         pass
@@ -19,7 +23,7 @@ class rootSolver:
         
         result = math.inf 
         x = None
-
+        
         while not -self.precision < result < self.precision:
             if x != None and self.areValidBounds(x, self.b):
                 self.a = x
@@ -30,6 +34,7 @@ class rootSolver:
 
             x = (self.a + self.b) / 2
             result = self.f(x)
+        
 
         return result
 
@@ -38,7 +43,7 @@ class rootSolver:
         pass
 
     def areValidBounds(self, a, b):
-        return self.f(a) * self.f(b)  < 0 
+        return self.f(a) * self.f(b) < 0 
 
     def f(self, x):
         return x * x * x;
@@ -47,14 +52,32 @@ class rootSolver:
         print()
 
 def main():
-    print("Hello please pick one of the following functions!:")
-    print("1) x^3 - 2x^2 + 3x - 1")
-    print("2) sin(x) + cos(x)")
-    print("3) 2^x - 1/e")
-    print("4) e^x * sin(2x) + e^x")
-    picked = input()
+    print("Hello please pick one of the following functions!:" + 
+        "\n1) x^3 - 2x^2 + 3x - 1" +
+        "\n2) sin(x) + cos(x)" +
+        "\n3) 2^x - 1/e" +
+        "\n4) e^x * sin(2x) + e^x")
 
-    solver = rootSolver(-5, 2, 3, 0.5)
+    functionNr = int(input())
+
+    print("Input left bound (a)")
+    a = int(input())
+
+    print("Input right bound (b)")
+    b = int(input())
+
+    print("Choose stop condition: " +
+          "\n1) by precision" + 
+          "\n2) by iteration")
+    stopTypeNr = int(input())
+
+    if stopTypeNr == 1:
+        iterOrPrecision = float(input("Input precision"))
+    elif stopTypeNr == 2:
+        iterOrPrecision = int(input("Input iteration count")) 
+
+    #solver = rootSolver(-5, 2, 3, 0.5)
+    solver = rootSolver(functionNr, a, b, stopTypeNr, iterOrPrecision)
     print(solver.bisectionMethod())
 
 main()
